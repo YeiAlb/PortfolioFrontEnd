a34b99a5a417810a189f82e7b84a404d01328f95
YeiAlb <mr.yeimeralberto@gmail.com>
2022-10-13 23:32:00 -0300

    Configurando archivo de tsconfig.json

--- Maquetado_Estatico/app.ts
+++ Maquetado_Estatico/app.ts
@@ -1,2 +1,14 @@
-let message: string = 'Hello World';
-console.log(message);
+let message: string = "Hello World!";
+
+const userName = "Yei_Alb";
+const age = 40;
+
+console.log(`${message}, my name is ${userName}, I'm ${age} years old.`);
+
+let myTypeString: string = 'Hello World!!';
+let myTypeNumber: number = 40;
+let myTypeBoolean: boolean = true;
+
+//array
+
+let arrNumber: number[] = [1, 2, 3];
---
+++ Maquetado_Estatico/dist/app.js
@@ -0,0 +1,5 @@
+"use strict";
+let message = "Hello World!";
+const userName = "Yei_Alb";
+const age = 40;
+console.log(`${message}, my name is ${userName}, I'm ${age} years old.`);

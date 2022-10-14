d1b64a9353c6b8e2f5c72b96aec79695dc75d2bf
YeiAlb <mr.yeimeralberto@gmail.com>
2022-10-14 00:48:53 -0300

    Configuración del npm http-server

--- Maquetado_Estatico/app.ts
+++ Maquetado_Estatico/app.ts
@@ -1,3 +1,6 @@
+//Autoguardado de TS
+//comando ==>tsc / indicamos el directorio, RECORDAR LOS GUIONES --outDir/ qué fichero queremos vigilar (dist) y luego el comando --watch
+//tsc --outDir dist app.ts --watch
 let message: string = "Hello World!";
 
 const userName = "Yei_Alb";
@@ -9,6 +12,30 @@ let myTypeString: string = 'Hello World!!';
 let myTypeNumber: number = 40;
 let myTypeBoolean: boolean = true;
 
-//array
-
+//array : de tipo number = y asignamos. 
+//Primera forma
 let arrNumber: number[] = [1, 2, 3];
+// Sería la misma declaración de array.
+//Segunda forma.
+let arrNumber2: Array<number> = [0, 1, 2, 3];
+
+//Si intentara añadir al siguiente array o a los anteriores asignaciones diferentes a su tipo no me dejaría.
+let arrStrings: Array<string> = ['Fútbol', 'Psicología', 'Teología', 'Pensamiento', 'Familia'];
+
+//Aunque si se podría en el siguiente array
+let arrAny: Array<any> = ['San Pablo', 'Queniquea', 1, 2, true];
+//le resta autoridad a TS pero puede ayudar pero mejor usarlo sólo lo menos posible.
+
+//Tuple
+let tuplePlayers: [string, number, boolean] = ['Messi', 10, true];
+
+//Tuple Array. Estaré usando la primera forma==>
+let players: [number, string][];
+//Tiene que darse que lo primero es un número y luego un array.
+players = [
+    [1, 'Messi'],
+    [2, 'Cristiano'],
+    [3, 'Lebron']
+];
+
+//INFERENCIA DE TIPOS.
--- Maquetado_Estatico/dist/app.js
+++ Maquetado_Estatico/dist/app.js
@@ -1,5 +1,32 @@
-"use strict";
-let message = "Hello World!";
-const userName = "Yei_Alb";
-const age = 40;
-console.log(`${message}, my name is ${userName}, I'm ${age} years old.`);
+//Autoguardado de TS
+//comando ==>tsc / indicamos el directorio, RECORDAR LOS GUIONES --outDir/ qué fichero queremos vigilar (dist) y luego el comando --watch
+//tsc --outDir dist app.ts --watch
+var message = "Hello World!";
+var userName = "Yei_Alb";
+var age = 40;
+console.log("".concat(message, ", my name is ").concat(userName, ", I'm ").concat(age, " years old."));
+var myTypeString = 'Hello World!!';
+var myTypeNumber = 40;
+var myTypeBoolean = true;
+//array : de tipo number = y asignamos. 
+//Primera forma
+var arrNumber = [1, 2, 3];
+// Sería la misma declaración de array.
+//Segunda forma.
+var arrNumber2 = [0, 1, 2, 3];
+//Si intentara añadir al siguiente array o a los anteriores asignaciones diferentes a su tipo no me dejaría.
+var arrStrings = ['Fútbol', 'Psicología', 'Teología', 'Pensamiento', 'Familia'];
+//Aunque si se podría en el siguiente array
+var arrAny = ['San Pablo', 'Queniquea', 1, 2, true];
+//le resta autoridad a TS pero puede ayudar pero mejor usarlo sólo lo menos posible.
+//Tuple
+var tuplePlayers = ['Messi', 10, true];
+//Tuple Array. Estaré usando la primera forma==>
+var players;
+//Tiene que darse que lo primero es un número y luego un array.
+players = [
+    [1, 'Messi'],
+    [2, 'Cristiano'],
+    [3, 'Lebron']
+];
+//INFERENCIA DE TIPOS.
